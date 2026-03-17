d11046b1d6832545105a20e87e135eb58b09899c
kaushal raj <kaushal2020ssm@gmail.com>
2026-03-17 17:54:20 +0530

    Add frontend project structure and login module implementation

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: 'auth',
+    children: [{ path: 'login', component: Login }],
+  },
+  { path: 'login', redirectTo: 'auth/login', pathMatch: 'full' },
+];
---
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  email: string = '';
+  password: string = '';
+
+  onLogin() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
