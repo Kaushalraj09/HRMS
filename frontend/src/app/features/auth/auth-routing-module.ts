6c9fa385811c684ffdb6aa10b0caf9af9e67acb5
kaushal raj <kaushal2020ssm@gmail.com>
2026-03-24 18:07:49 +0530

    feat: added all component in hr-dashboard

--- frontend/src/app/features/auth/auth-routing-module.ts
+++ frontend/src/app/features/auth/auth-routing-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HrDashboardComponent } from '../hr-dashboard/hrDashboard.component';
+import { EmpDashboardComponent } from '../emp-dashboard.component/emp-dashboard.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'hr-dashboard', component: HrDashboardComponent}
+  {path: 'hr-dashboard', component: HrDashboardComponent},
+  {path: 'emp-dashboard', component: EmpDashboardComponent}
+
 ];
 
 @NgModule({
